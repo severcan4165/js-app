f6c8170ba121d47a32b51d0b1d8b51244cf47134
severcan4165 <salihsevercann@gmail.com>
2022-08-19 22:18:27 +0300

    edit

--- hesapmakinem/hesapmakinesi.js
+++ hesapmakinem/hesapmakinesi.js
@@ -28,3 +28,34 @@ function hesapla() {
     }
 
 }
+
+//! Other simple solution
+// let f = +prompt(`ilk`);
+// let s = +prompt(`sec`);
+// let t = +prompt(`third`);
+// let result = f%10==s%10 || f%10==t%10 || t%10==s%10;
+// console.log(result);
+
+// # ODEV1: Dort Islem Hesap Makinasi (2 Sayı bir operator)
+
+// let sayi1 = +prompt("ilk sayıyı giriniz");
+// let islem = prompt("işlemi giriniz");
+// let sayi2 = +prompt("ilk sayıyı giriniz");
+
+
+// if(islem == "+"){
+//     let sonuc = sayi1 + sayi2;
+//     console.log(sonuc);
+// }
+// if(islem== "*"){
+//     let sonuc = sayi1 * sayi2;
+//     console.log(sonuc);
+// }
+// if(islem== "-"){
+//     let sonuc = sayi1 - sayi2;
+//     console.log(sonuc);
+// }
+// if(islem== "/"){
+//     let sonuc = sayi1 / sayi2;
+//     console.log(sonuc);
+// }
