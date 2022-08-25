10e1307afa0cf8a94e00fd7d2f95d8a6b6500cb2
severcan4165 <salihsevercann@gmail.com>
2022-08-25 19:20:27 +0300

    ..

---
+++ recursive_faktöriyel/faktoriyel.js
@@ -0,0 +1,16 @@
+const factorial = 1;
+const n =Number(prompt("sayı girin"));
+const func = (x) =>{
+  if(x<1){
+
+  }
+  else if(x==1){
+    return 1;
+  }
+  else{
+    return x*func(x-1);
+  }
+
+}
+
+console.log(func(n));
