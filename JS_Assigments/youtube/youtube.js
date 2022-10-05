bb3d085c2cd43c7b3febc29a6079af4f5b46ee0f
salih <salihsevercann@gmail.com>
2022-10-05 13:01:06 +0300

    edit

--- JS_Assigments/youtube/youtube.js
+++ JS_Assigments/youtube/youtube.js
@@ -5,7 +5,7 @@
 
 const link1 = "https://www.youtube.com/watch?v=b7vfp5G4brE";
 
-let kisalink= (link)=>{
+let kisalink= (link1)=>{
     let indx = link1.indexOf("=");
     let kisa = link1.slice(indx+1);
         return "https://youtu.be/" + kisa;
